e6d973f509be909b4f307a7543a56129f16c8323
Medi Amiri <s.mahdiamiri@outlook.com>
2023-05-12 17:01:06 +0200

    JSON Sample all of them are just one Objects

---
+++ JSON Sample all of them are just one Objects.js
@@ -0,0 +1,18 @@
+myObj =  {"Student" : [
+   { 
+      "id":"01", 
+      "name": "Tom", 
+      "lastname": "Price" 
+   }, 
+   { 
+      "id":"02", 
+      "name": "Nick", 
+      "lastname": "Thameson" 
+   }  
+]
+}   
+
+
+
+
+console.log(myObj.Student[1].name);
