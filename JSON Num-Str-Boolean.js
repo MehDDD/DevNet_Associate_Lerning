064b64694a3d9f0d5b71800478cf01c537e2ce35
Medi Amiri <s.mahdiamiri@outlook.com>
2023-05-13 11:53:11 +0200

    add console log to show data types

---
+++ JSON Num-Str-Boolean.js
@@ -0,0 +1,28 @@
+Obj = {"student" : [
+    {
+        "ID" :1 ,
+        "FirstName" : "Medi",
+        "LastName" : "Amiri",
+        "Birthdate" : "04.04.1369",
+        "Active" : true
+    },
+    {
+        "ID" : "2",
+        "FirstName" : "Medi2",
+        "LastName" : "Amiri2",
+        "Birthdate" : "05.05.1369",
+        "Active" : false
+    },
+    {
+        "ID" : "3",
+        "FirstName" : "Medi3",
+        "LastName" : "Amiri3",
+        "Birthdate" : "06.06.1369",
+        "Active" : false
+    }
+]
+} 
+
+
+console.log(Obj.student[2])
+console.log(typeof Obj.student[0].Active)
