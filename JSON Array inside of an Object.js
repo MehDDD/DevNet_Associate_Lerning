feb316e1d2c067546ffa3f367982cfa98a619b73
Medi Amiri <s.mahdiamiri@outlook.com>
2023-05-13 12:55:12 +0200

    JSON Array inside of an Object

---
+++ JSON Array inside of an Object.js
@@ -0,0 +1,23 @@
+Obj = {"student" : [
+    {
+        "ID" :1 ,
+        "Name" : ["Medi", "Amiri"],
+        "Birthdate" : "04.04.1369",
+        "Active" : true
+    },
+    {
+        "ID" : "2",
+        "Name" : ["Medi2", "Amiri2"],
+        "Birthdate" : "05.05.1369",
+        "Active" : false
+    },
+    {
+        "ID" : "3",
+        "Name" : ["Medi3", "Amiri3"],
+        "Birthdate" : "06.06.1369",
+        "Active" : false
+    }
+]
+} 
+
+console.log(Obj.student[2].Name[1])
