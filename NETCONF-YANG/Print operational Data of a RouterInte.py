ce4420326baa75d4366a07a6f5e084aee0f1dc48
Medi Amiri <s.mahdiamiri@outlook.com>
2023-07-25 00:31:35 +0200

    Print operational Data of a RouterInterface

--- NETCONF-YANG/Print operational Data of a RouterInte.py
+++ NETCONF-YANG/Print operational Data of a RouterInte.py
@@ -1,5 +1,7 @@
 # Print operational Data of a RouterInterface
 from ncclient import manager
+import xml.dom.minidom
+
 # You get the LoginInformation from CiscoDevNetSandbox
 router = {"host": "10.10.20.48", "port": "830", "username": "developer", "password": "C1sco12345"}
 # This is the Netconf filter, to show the GigabitEthernet2 Information. This is an XML Structure.
@@ -17,3 +19,16 @@ netconf_filter = """
   </interfaces-state>
 </filter>
 """
+
+with manager.connect(host=router["host"], port=router["port"], username=router["username"], password=router["password"], hostkey_verify=False) as m:
+    for capability in m.server_capabilities:
+        print('*' * 50)
+        print(capability)
+
+        # now we get the netconf data
+        interface_netconf = m.get(netconf_filter)
+        # we turn the response to a human readable xml data
+        xmlDom = xml.dom.minidom.parseString(str(interface_netconf))
+        print(xmlDom.toprettyxml(indent=" ")
+              print('*' * 50 + 'break' + '*' * 50)
+    m.close_session()
