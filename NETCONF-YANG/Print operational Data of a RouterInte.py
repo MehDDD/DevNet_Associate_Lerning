e9e578bfdce304815c617174da52bff30474d33e
Medi Amiri <s.mahdiamiri@outlook.com>
2023-07-25 00:21:46 +0200

    Print operational Data of a RouterInterface

--- NETCONF-YANG/Print operational Data of a RouterInte.py
+++ NETCONF-YANG/Print operational Data of a RouterInte.py
@@ -2,7 +2,7 @@
 from ncclient import manager
 # You get the LoginInformation from CiscoDevNetSandbox
 router = {"host": "10.10.20.48", "port": "830", "username": "developer", "password": "C1sco12345"}
-# This is the Netconf filter, to show the GigabitEthernet2 Information. This is an XML Structure
+# This is the Netconf filter, to show the GigabitEthernet2 Information. This is an XML Structure.
 netconf_filter = """
 <filter>
   <interfaces xmlns="urn:ietf:params:xml:ns:yang:ietf-interfaces">
